6fc087d8e2282365d132e0f98ba8d27be7d03502
kumar chaudhary <chaudharykumar228@gmail.com>
2024-10-30 22:14:49 +0530

    Added AttendanceData Component

---
+++ frontend/src/Components/AttendanceData/AttendanceData.jsx
@@ -0,0 +1,33 @@
+import React, { useContext} from "react";
+import "./AttendanceData.css";
+
+import { StoreContext } from "../../context/StoreContext";
+
+const AttendanceData = () => {
+  const {data}= useContext(StoreContext);
+
+  return (
+    <div className="attendance-data-container">
+      <div className="table-container">
+        <thead className="table-heading">
+          <tr>
+            <th>Name</th>
+            <th>Roll Number</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody className="table-data">
+          {data.map((student, index)=>(
+            <tr key={index} className="table-row-data">
+            <td>{student.name}</td>
+            <td>{student.roll_number}</td>
+            <td>{student.status}</td>
+          </tr>
+          ))}
+        </tbody>
+      </div>
+    </div>
+  );
+};
+
+export default AttendanceData;
