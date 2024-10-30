d9dab7b966e87b6fc2f0f4876cc2a6be56fc137c
kumar chaudhary <chaudharykumar228@gmail.com>
2024-10-30 22:10:01 +0530

    Updated Code

--- backend/controllers/SaveImageController.js
+++ backend/controllers/SaveImageController.js
@@ -1,18 +1,23 @@
-// import saveModel from "../modals/SaveImageModel";
-const saveModel = require("../modals/SaveImageModel");
+const saveModel = require("../modals/SaveImageModel.js");
 
 const saveImageDB = async (req, resp) => {
-  const Image_fileName = `${req.file.filename}`;
+  if (!req.files || req.files.length === 0) {
+    return resp.json({ success: false, message: "No image files uploaded" });
+  }
+
+  const imageList = req.files.map((file) => file.filename);
+  console.log("Image List:", imageList); 
 
   const saveImage = new saveModel({
-    saveImg: Image_fileName,
+    saveImage: imageList, 
   });
 
   try {
     await saveImage.save();
     resp.json({ success: true, message: "Image saved successfully to DB" });
   } catch (error) {
-    resp.json({ success: false, message: "Error" });
+    console.error("Error saving image to database:", error);
+    resp.json({ success: false, message: "Error saving image to database" });
   }
 };
 
