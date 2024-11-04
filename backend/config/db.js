80ce075a64029b795f361651ba697b689a04f2cb
kumar chaudhary <chaudharykumar228@gmail.com>
2024-11-04 23:12:45 +0530

    Optimize code for db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 // Use CommonJS require instead of import
 const mongoose = require("mongoose");
-
+console.log("mongo url is", process.env.MONGODB_URI);
 const ConnectDB = async () => {
   await mongoose
-    .connect("mongodb://localhost:27017/Attendance_system")
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("DB Connected"));
 };
 
