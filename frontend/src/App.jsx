15a9d99eb58a00dd88a3ac9d700811a065aafcdf
Kumar Chaudhary <115098236+kumarchy@users.noreply.github.com>
2025-04-03 18:03:07 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,15 +8,11 @@ function App() {
   return (
     <div className="attendance-app">
       <header>
-        {/* Show the SignInButton when the user is signed out */}
         <SignedOut>
-          {/* <SignIn redirectUrl="/" /> */}
           <SignInPage redirectUrl="/" />
         </SignedOut>
 
-        {/* Show the UserButton and Attendance component when the user is signed in */}
         <SignedIn>
-          {/* <UserButton /> */}
           <Attendance />
         </SignedIn>
       </header>
